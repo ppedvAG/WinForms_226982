d6aa9755656e8be991098d10cc9e74f64b3f3bcb
André <AndreR@ppedv.de>
2023-05-17 09:37:05 +0200

    Try Catch mit finally

--- HalloWinForms/HalloWinForms/CustomerManagement.vb
+++ HalloWinForms/HalloWinForms/CustomerManagement.vb
@@ -1,34 +1,48 @@
 ﻿Imports System.Data.SqlClient
+Imports System.Diagnostics.Eventing
 
 Public Class CustomerManagement
     Private Sub loadButton_Click(sender As Object, e As EventArgs) Handles loadButton.Click
 
         'TODO rrror handling
         'Todo Dispose
+        Dim con As SqlConnection
+        Try
+            Dim conString = "Server=(localdb)\mssqllocaldb;Database=Northwnd;Trusted_Connection=true"
 
-        Dim conString = "Server=(localdb)\mssqllocaldb;Database=Northwnd;Trusted_Connection=true"
+            con = New SqlConnection(conString)
+            con.Open()
 
-        Dim con = New SqlConnection(conString)
-        con.Open()
+            Dim cmd = New SqlCommand("SELECT * FROM Customerserölkmfgrlökmlerkmglkerm")
+            cmd.Connection = con
 
-        Dim cmd = New SqlCommand("SELECT * FROM Customers")
-        cmd.Connection = con
+            Dim reader = cmd.ExecuteReader()
 
-        Dim reader = cmd.ExecuteReader()
+            Dim customers As New List(Of Customer)
 
-        Dim customers As New List(Of Customer)
+            While reader.Read()
 
-        While reader.Read()
+                Dim newCust = New Customer()
+                newCust.Id = reader.GetString(reader.GetOrdinal("CustomerID"))
+                newCust.CompanyName = reader.GetString(reader.GetOrdinal("CompanyName"))
+                newCust.ContactName = reader.GetString(reader.GetOrdinal("ContactName"))
+                customers.Add(newCust)
 
-            Dim newCust = New Customer()
-            newCust.Id = reader.GetString(reader.GetOrdinal("CustomerID"))
-            newCust.CompanyName = reader.GetString(reader.GetOrdinal("CompanyName"))
-            newCust.ContactName = reader.GetString(reader.GetOrdinal("ContactName"))
-            customers.Add(newCust)
+            End While
 
-        End While
 
-        DataGridView1.DataSource = customers
+            DataGridView1.DataSource = customers
+
+        Catch ex As SqlException
+            MessageBox.Show($"SQL ERORR: {ex.Message}", ":-(", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Catch ex As Exception
+            MessageBox.Show($"Error: {ex.Message}", ":-(", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Finally
+            If con Is Not Nothing Then
+                con.Close()
+            End If
+
+        End Try
 
     End Sub
 End Class
